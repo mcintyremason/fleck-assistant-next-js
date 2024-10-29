856050d388fd7ce877637e04e56114c89de385ed
mahun <mcintyre.mlm@gmail.com>
2024-10-29 11:17:09 -0400

    add meta

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -20,9 +20,9 @@ const Layout = ({
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="manifest" href="/manifest.json" />
-      <link rel="apple-touch-icon" href="/rwf-180x180.png" />
-      <link rel="apple-touch-startup-image" href="/rwf-180x180.png" />
-      <link rel="icon" type="image/png" href="/img/icons/favicon-1/rwf.png" />
+      <link rel="icon" href="/rwf.png" type="image/png" />
+      <link rel="shortcut icon" href="/rwf.png" type="image/png" />
+      <link rel="apple-touch-icon" href="/rwf180.png" type="image/png" />
       <meta name="apple-mobile-web-app-title" content="Fleck Assistant" />
       <meta name="description" content="More than only a website..." />
       <link type="text/css" rel="preload" as="style" href="/css/app.css" />
